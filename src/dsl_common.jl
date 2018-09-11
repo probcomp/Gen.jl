02e24fb603f5d14b748e503282bfaf640ccab5e9
Marco Cusumano-Towner <imarcoam@gmail.com>
2018-09-10 21:45:45 -0400

    utility for stripping code of line info

--- src/dsl_common.jl
+++ src/dsl_common.jl
@@ -42,6 +42,33 @@ function strip_marked_for_ad(arg::Expr)
     end
 end
 
+
+##############################
+# utility for examining code #
+##############################
+
+function strip_lineinfo(expr::Expr)
+    @assert !(expr.head == :line)
+    new_args = []
+    for arg in expr.args
+        if (isa(arg, Expr) && arg.head == :line) || isa(arg, LineNumberNode)
+        elseif isa(arg, Expr) && arg.head == :block
+            stripped = strip_lineinfo(arg)
+            append!(new_args, stripped.args)
+        else
+            push!(new_args, strip_lineinfo(arg))
+        end
+    end
+    Expr(expr.head, new_args...)
+end
+
+function strip_lineinfo(expr)
+    expr
+end
+
+export strip_lineinfo
+
+
 export @addr
 export @read
 export @splice
