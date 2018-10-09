48a90fa60f5ad51cdd056f3bbe7a886ddf73d551
Marco Cusumano-Towner <imarcoam@gmail.com>
2018-10-09 12:47:31 -0400

    Add minimal support for backprop_trace and plate

--- src/generators/plate_of_dists/plate_of_dists.jl
+++ src/generators/plate_of_dists/plate_of_dists.jl
@@ -11,9 +11,8 @@ struct PlateOfDist{T} <: Generator{PersistentVector{T},VectorDistTrace{T}}
     kernel::Distribution{T}
 end
 
-# TODO
-#accepts_output_grad(plate::Plate) = false
-#has_argument_grads(plate::Plate) = has_argument_grads(plate.kernel)
+accepts_output_grad(plate::PlateOfDist) = false # TODO
+has_argument_grads(plate::PlateOfDist) = has_argument_grads(plate.kernel)
 
 function plate(kernel::Distribution{T}) where {T}
     PlateOfDist{T}(kernel)
@@ -95,6 +94,54 @@ function update(gen::PlateOfDist{T}, new_args, args_change::Nothing, trace::Vect
     call = CallRecord(score, values, new_args)
     new_trace = Gen.VectorDistTrace{T}(values, call)
     (new_trace, weight, EmptyAssignment(), nothing)
+end
+
+# TODO handle other selection types
+function backprop_trace(gen::PlateOfDist{T}, trace::VectorDistTrace{T}, selection::EmptyAddressSet, retval_grad::Nothing) where {T}
+    call = get_call_record(trace)
+    args = call.args
+    n_args = length(args)
+    len = length(args[1])
+    
+    has_grads = has_argument_grads(gen.kernel)
+    arg_grad = Vector(undef, n_args)
+    for (i, has_grad) in enumerate(has_grads)
+        if has_grad
+            arg_grad[i] = Vector(undef, len)
+        else
+            arg_grad[i] = nothing
+        end
+    end
+    
+    #value_trie = DynamicAssignment()
+    #gradient_trie = DynamicAssignment()
+    
+    for key=1:len
+        value = trace.values[key]
+        kernel_args = get_args_for_key(args, key)
+        #subtrace = trace.subtraces[key]
+        #if has_internal_node(selection, key)
+            #sub_selection = get_internal_node(selection, key)
+        #else
+            #sub_selection = EmptyAddressSet()
+        #end
+        #kernel_retval_grad = (retval_grad == nothing) ? nothing : retval_grad[key]
+        kernel_grads = logpdf_grad(gen.kernel, value, kernel_args...)
+        value_grad = kernel_grads[1]
+        kernel_arg_grad = (kernel_grads[2:end]...,)
+        #(kernel_arg_grad::Tuple, kernel_value_trie, kernel_gradient_trie) = backprop_trace(
+            #gen.kernel, subtrace, sub_selection, kernel_retval_grad)
+        #set_internal_node!(value_trie, key, kernel_value_trie)
+        #set_internal_node!(gradient_trie, key, kernel_gradient_trie)
+        for (i, grad, has_grad) in zip(1:n_args, kernel_arg_grad, has_grads)
+            if has_grad
+                arg_grad[i][key] = grad
+            end
+        end
+    end
+    value_trie = EmptyAssignment()
+    gradient_trie = EmptyAssignment()
+    ((arg_grad...,), value_trie, gradient_trie)
 end
 
 export plate
