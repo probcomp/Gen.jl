9dca0ffdf852e60ecadac4c9cb6f1ffab13f3391
Marco Cusumano-Towner <imarcoam@gmail.com>
2018-10-31 01:13:21 -0400

    Minimal inverse planning demo running

--- examples/planning/demo.jl
+++ examples/planning/demo.jl
@@ -2,20 +2,24 @@ include("scenes.jl")
 include("path_planner.jl")
 include("model.jl")
 
+using Printf: @sprintf
 import Random
 
 function make_scene()
-    scene = Scene(0, 100, 0, 100) 
-    add!(scene, Tree(Point(30, 20)))
-    add!(scene, Tree(Point(83, 80)))
-    add!(scene, Tree(Point(80, 40)))
-    wall_height = 30.
+    scene = Scene(0, 1, 0, 1) 
+    add!(scene, Tree(Point(0.30, 0.20), size=0.1))
+    add!(scene, Tree(Point(0.83, 0.80), size=0.1))
+    add!(scene, Tree(Point(0.80, 0.40), size=0.1))
+    horiz = 1
+    vert = 2
+    wall_height = 0.30
+    wall_thickness = 0.02
     walls = [
-        Wall(Point(20., 40.), 1, 40., 2., wall_height)
-        Wall(Point(60., 40.), 2, 40., 2., wall_height)
-        Wall(Point(60. - 15., 80.), 1, 15. + 2., 2., wall_height)
-        Wall(Point(20., 80.), 1, 15., 2., wall_height)
-        Wall(Point(20., 40.), 2, 40., 2., wall_height)]
+        Wall(Point(0.20, 0.40), horiz, 0.40, wall_thickness, wall_height)
+        Wall(Point(0.60, 0.40), vert, 0.40, wall_thickness, wall_height)
+        Wall(Point(0.60 - 0.15, 0.80), horiz, 0.15 + wall_thickness, wall_thickness, wall_height)
+        Wall(Point(0.20, 0.80), horiz, 0.15, wall_thickness, wall_height)
+        Wall(Point(0.20, 0.40), vert, 0.40, wall_thickness, wall_height)]
     for wall in walls
         add!(scene, wall)
     end
@@ -36,38 +40,35 @@ function show_prior_samples()
     end
     savefig("demo.png")
 end
-# generate_prior_samples()
+show_prior_samples()
 
-@gen function start_proposal()
-    @addr(uniform(0, 1), :start_x)
-    @addr(uniform(0, 1), :start_y)
-end
-
-@gen function stop_proposal()
+@gen function stop_proposal(prev_trace)
     @addr(uniform(0, 1), :stop_x)
     @addr(uniform(0, 1), :stop_y)
 end
 
-@gen function speed_proposal()
+@gen function speed_proposal(prev_trace)
     @addr(uniform(0, 1), :speed)
 end
 
-@gen function noise_proposal()
+@gen function noise_proposal(prev_trace)
     @addr(uniform(0, 1), :noise)
 end
 
-function inference(measurements::Vector{Point}, iters::Int)
-    constraints = Assignment()
+function inference(measurements::Vector{Point}, start::Point, iters::Int)
+    t = length(measurements)
+
+    constraints = DynamicAssignment()
     for (i, pt) in enumerate(measurements)
         constraints[i => :x] = pt.x
         constraints[i => :y] = pt.y
     end
+    constraints[:start_x] = start.x
+    constraints[:start_y] = start.y
 
-    (trace, _) = generate(model, (scene, times), constraints)
+    (trace, _) = generate(model, (scene, times[1:t]), constraints)
 
     for iter=1:iters
-        println("iter: $iter")
-        trace = mh(model, start_proposal, (), trace)
         trace = mh(model, stop_proposal, (), trace)
         trace = mh(model, speed_proposal, (), trace)
         trace = mh(model, noise_proposal, (), trace)
@@ -96,6 +97,25 @@ function experiment()
     assignment = get_assignment(trace)
     measurements = [Point(assignment[i => :x], assignment[i => :y]) for i=1:length(times)]
     start = Point(assignment[:start_x], assignment[:start_y])
+
+    for t=1:length(times)
+    #for t in [length(times)]
+        println("t: $t")
+        traces = []
+        for i=1:100
+            println(i)
+            trace = inference(measurements[1:t], start, 100)
+            push!(traces, trace)
+        end
+        figure(figsize=(4, 4))
+        ax = gca()
+        for (i, trace) in enumerate(traces)
+            render(scene, trace, ax; show_measurements=i>1, show_start=i>1,
+                   show_path=false, show_noise=false, stop_alpha=0.2, path_alpha=0.2)
+        end
+        fname = @sprintf("inferred_%03d.png", t)
+        savefig(fname)
+    end
 end
 
 experiment()
--- examples/planning/model.jl
+++ examples/planning/model.jl
@@ -5,28 +5,21 @@ using PyPlot
 
 @gen function model(scene::Scene, times::Vector{Float64})
 
-    # dimensions of the scene
-    xmin = scene.xmin
-    xmax = scene.xmax
-    ymin = scene.ymin
-    ymax = scene.ymax
-    dimension = max(abs(xmax - xmin), abs(ymax - ymin))
-
     # start point of the agent
-    start_x = xmin + (xmax - xmin) * @addr(uniform(0, 1), :start_x)
-    start_y = ymin + (ymax - ymin) * @addr(uniform(0, 1), :start_y)
+    start_x = @addr(uniform(0, 1), :start_x)
+    start_y = @addr(uniform(0, 1), :start_y)
     start = Point(start_x, start_y)
 
     # stop point of the agent
-    stop_x = xmin + (xmax - xmin) * @addr(uniform(0, 1), :stop_x)
-    stop_y = xmin + (ymax - ymin) * @addr(uniform(0, 1), :stop_y)
+    stop_x = @addr(uniform(0, 1), :stop_x)
+    stop_y = @addr(uniform(0, 1), :stop_y)
     stop = Point(stop_x, stop_y)
 
     # plan a path that avoids obstacles in the scene
-    maybe_path = plan_path(start, stop, scene, PlannerParams(2000, 3.0, 10000, 1.))
+    maybe_path = plan_path(start, stop, scene, PlannerParams(300, 3.0, 2000, 1.))
     
     # speed
-    speed = dimension * @addr(uniform(0, 1), :speed)
+    speed = @addr(uniform(0, 1), :speed)
 
     # walk path at constant speed
     if !isnull(maybe_path)
@@ -37,7 +30,7 @@ using PyPlot
     end
 
     # generate noisy observations
-    noise = dimension * 0.1 * @addr(uniform(0, 1), :noise)
+    noise = 0.1 * @addr(uniform(0, 1), :noise)
     for (i, point) in enumerate(locations)
         @addr(normal(point.x, noise), i => :x)
         @addr(normal(point.y, noise), i => :y)
@@ -46,14 +39,22 @@ using PyPlot
     (start, stop, speed, noise, maybe_path, locations)
 end
 
-function render(scene::Scene, trace, ax)
+function render(scene::Scene, trace, ax;
+                show_measurements=true,
+                show_start=true, show_stop=true,
+                show_path=true, show_noise=true,
+                start_alpha=1., stop_alpha=1., path_alpha=1.)
     (start, stop, speed, noise, maybe_path, locations) = get_call_record(trace).retval
     assignment = get_assignment(trace)
     render(scene, ax)
     sca(ax)
-    scatter([start.x], [start.y], color="blue", s=100)
-    scatter([stop.x], [stop.y], color="red", s=100)
-    if !isnull(maybe_path)
+    if show_start
+        scatter([start.x], [start.y], color="blue", s=100, alpha=start_alpha)
+    end
+    if show_stop
+        scatter([stop.x], [stop.y], color="red", s=100, alpha=stop_alpha)
+    end
+    if !isnull(maybe_path) && show_path
         path = get(maybe_path)
         for i=1:length(path.points)-1
             prev = path.points[i]
@@ -63,17 +64,19 @@ function render(scene::Scene, trace, ax)
     end
 
     # plot locations with measurement noise around them
-    scatter([pt.x for pt in locations], [pt.y for pt in locations], color="orange", alpha=1., s=25)
-    for pt in locations
-        circle = patches[:Circle]((pt.x, pt.y), noise, facecolor="purple", alpha=0.2)
-        ax[:add_patch](circle)
+    scatter([pt.x for pt in locations], [pt.y for pt in locations],
+        color="orange", alpha=path_alpha, s=25)
+    if show_noise
+        for pt in locations
+            circle = patches[:Circle]((pt.x, pt.y), noise, facecolor="purple", alpha=0.2)
+            ax[:add_patch](circle)
+        end
     end
     
     # plot measured locations
-    measured_xs = [assignment[i => :x] for i=1:length(locations)]
-    measured_ys = [assignment[i => :y] for i=1:length(locations)]
-    scatter(measured_xs, measured_ys, marker="x", color="black", alpha=1., s=25)
+    if show_measurements
+        measured_xs = [assignment[i => :x] for i=1:length(locations)]
+        measured_ys = [assignment[i => :y] for i=1:length(locations)]
+        scatter(measured_xs, measured_ys, marker="x", color="black", alpha=1., s=25)
+    end
 end
-
-
-
