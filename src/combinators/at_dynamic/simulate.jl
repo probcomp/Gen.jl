24896336c493aa0987c355209a4cc9da0c6691e3
Marco Cusumano-Towner <imarcoam@gmail.com>
2018-11-28 14:30:37 -0500

    generate for AtDynamic

--- src/combinators/at_dynamic/simulate.jl
+++ src/combinators/at_dynamic/simulate.jl
@@ -5,3 +5,17 @@ function simulate(gen::AtDynamic{T,U,K}, args) where {T,U,K}
     call = CallRecord{T}(sub_call.score, sub_call.retval, args)
     AtDynamicTrace{T,U,K}(call, subtrace, key)
 end
+
+function generate(gen::AtDynamic{T,U,K}, args, constraints) where {T,U,K}
+    (key::K, kernel_args) = args
+    if has_internal_node(constraints, key)
+        subconstraints = get_internal_node(constraints, key)
+    else 
+        subconstraints = EmptyAssignment()
+    end
+    (subtrace::U, weight) = generate(gen.kernel, kernel_args, subconstraints)
+    sub_call = get_call_record(subtrace)
+    call = CallRecord{T}(sub_call.score, sub_call.retval, args)
+    trace = AtDynamicTrace{T,U,K}(call, subtrace, key)
+    (trace, weight)
+end
