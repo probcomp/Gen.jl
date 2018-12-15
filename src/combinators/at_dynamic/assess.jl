ee04cec83df80cc86544840213733183e0a11b5b
Marco Cusumano-Towner <imarcoam@gmail.com>
2018-12-14 20:09:53 -0500

    Remove dead code

--- src/combinators/at_dynamic/assess.jl
+++
@@ -1,8 +0,0 @@
-function assess(gen::AtDynamic{T,U,K}, args, constraints) where {T,U,K}
-    (key::K, kernel_args) = args
-    if length(get_internal_nodes(constraints)) + length(get_leaf_nodes(constraints)) > 1
-        error("Not all constraints were consumed")
-    end
-    kernel_constraints = get_internal_node(constraints, key)
-    assess(gen.kernel, kernel_args, kernel_constraints)
-end
