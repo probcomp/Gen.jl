a2bab7b7907135767efdb52fc9376793ccdbd8fb
Marco Cusumano-Towner <imarcoam@gmail.com>
2018-12-19 19:43:58 -0500

    Add quick start example

---
+++ examples/regression/quickstart.jl
@@ -0,0 +1,31 @@
+using Gen
+
+@gen function my_model(xs)
+    slope = @addr(normal(0, 2), :slope)
+    intercept = @addr(normal(0, 10), :intercept)
+    ys = Vector{Float64}(undef, length(xs))
+    for (i, x) in enumerate(xs)
+        ys[i] = @addr(normal(slope * x + intercept, 1), "y-$i")
+    end
+end
+
+function my_inference_program(xs, ys, num_iters)
+    constraints = DynamicAssignment()
+    for (i, y) in enumerate(ys)
+        constraints["y-$i"] = y
+    end
+    (trace, _) = initialize(my_model, (xs,), constraints)
+    slope_selection = select(:slope)
+    intercept_selection = select(:intercept)
+    for iter=1:num_iters
+        (trace, _) = default_mh(trace, slope_selection)
+        (trace, _) = default_mh(trace, intercept_selection)
+    end
+    assmt = get_assmt(trace)
+    return (assmt[:slope], assmt[:intercept])
+end
+
+xs = [1., 2., 3., 4., 5., 6., 7., 8., 9., 10.]
+ys = [8.23, 5.87, 3.99, 2.59, 0.23, -0.66, -3.53, -6.91, -7.24, -9.90]
+(slope, intercept) = my_inference_program(xs, ys, 1000)
+println("slope: $slope, intercept: $intercept")
--- examples/run_examples.jl
+++ examples/run_examples.jl
@@ -1,6 +1,7 @@
 using Gen
 using Test
 
+include("regression/quickstart.jl")
 include("regression/dynamic_map_optimize_gibbs.jl")
 include("regression/dynamic_mh.jl")
 include("regression/dynamic_resimulation_mh.jl")
