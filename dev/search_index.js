157ed0c9a1415a2d5240710ee1e823e29812cceb
autodocs <autodocs>
2018-12-29 06:15:18 +0000

    build based on 7800b84

--- dev/search_index.js
+++ dev/search_index.js
@@ -337,35 +337,35 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "ref/modeling/#Making-Random-Choices-1",
+    "location": "ref/modeling/#Making-random-choices-1",
     "page": "Modeling Languages",
-    "title": "Making Random Choices",
+    "title": "Making random choices",
     "category": "section",
     "text": "Random choices are made by applying a probability distribution to some arguments:val::Bool = bernoulli(0.5)See Probability Distributions for the set of built-in probability distributions. Within the context of a @gen function, wrapping the right-hand side of the expression associates the random choice with an address, and evaluates to the value of the random choice. Addresses can be any Julia value. Here, we give the Julia symbol address :z:val::Bool = @addr(bernoulli(0.5), :z)Not all random choices need to be given addresses. An address is required if the random choice will be observed, or will be referenced by a custom inference algorithm (e.g. if it will be proposed to by a custom proposal distribution).It is recommended to ensure that the support of a random choice at a given address (the set of values with nonzero probability or probability density) is constant across all possible executions of the generative function. This discipline will simplify reasoning about the probabilistic behavior of the function, and will help avoid difficult-to-debug NaNs or Infs from appearing. If the support of a random choice needs to change, consider using a different address for each distinct support."
 },
 
 {
-    "location": "ref/modeling/#Calling-Generative-Functions-1",
+    "location": "ref/modeling/#Calling-generative-functions-1",
     "page": "Modeling Languages",
-    "title": "Calling Generative Functions",
+    "title": "Calling generative functions",
     "category": "section",
     "text": "@gen functions can invoke other generative functions in three ways:Untraced call: If foo is a generative function, we can invoke foo from within the body of a @gen function using regular call syntax. The random choices made within the call are not given addresses in our trace, and are therefore non-addressable random choices (see Generative Function Interface for details on non-addressable random choices).val = foo(0.5)Traced call with shared address namespace: We can include the addressable random choices made by foo in the caller\'s trace using @splice:val = @splice(foo(0.5))Now, all random choices made by foo are included in our trace. The caller must guarantee that there are no address collisions.Traced call with a nested address namespace: We can include the addressable random choices made by foo in the caller\'s trace, under a namespace, using @addr:val = @addr(foo(0.5), :x)Now, all random choices made by foo are included in our trace, under the namespace :x. For example, if foo makes random choices at addresses :a and :b, these choices will have addresses :x => :a and :x => :b in the caller\'s trace."
 },
 
 {
-    "location": "ref/modeling/#Using-Composite-Addresses-1",
+    "location": "ref/modeling/#Using-composite-addresses-1",
     "page": "Modeling Languages",
-    "title": "Using Composite Addresses",
+    "title": "Using composite addresses",
     "category": "section",
     "text": "In Julia, Pair values can be constructed using the => operator. For example, :a => :b is equivalent to Pair(:a, :b) and :a => :b => :c is equivalent to Pair(:a, Pair(:b, :c)). A Pair value (e.g. :a => :b => :c) can be passed as the address field in an @addr expression, provided that there is not also a random choice or generative function called with @addr at any prefix of the address.Consider the following examples.This example is invalid because :a => :b is a prefix of :a => :b => :c:@addr(normal(0, 1), :a => :b => :c)\n@addr(normal(0, 1), :a => :b)This example is invalid because :a is a prefix of :a => :b => :c:@addr(normal(0, 1), :a => :b => :c)\n@addr(normal(0, 1), :a)This example is invalid because :a => :b is a prefix of :a => :b => :c:@addr(normal(0, 1), :a => :b => :c)\n@addr(foo(0.5), :a => :b)This example is invalid because :a is a prefix of :a => :b:@addr(normal(0, 1), :a)\n@addr(foo(0.5), :a => :b)This example is valid because :a => :b and :a => :c are not prefixes of one another:@addr(normal(0, 1), :a => :b)\n@addr(normal(0, 1), :a => :c)This example is valid because :a => :b and :a => :c are not prefixes of one another:@addr(normal(0, 1), :a => :b)\n@addr(foo(0.5), :a => :c)"
 },
 
 {
-    "location": "ref/modeling/#Return-Value-1",
+    "location": "ref/modeling/#Return-value-1",
     "page": "Modeling Languages",
-    "title": "Return Value",
+    "title": "Return value",
     "category": "section",
-    "text": "Like regular Julia functions, @gen functions return either the expression used in a return keyword, or by evaluating the last expression in the function body. Note that the return value of a @gen function is distinct from its trace."
+    "text": "Like regular Julia functions, @gen functions return either the expression used in a return keyword, or by evaluating the last expression in the function body. Note that the return value of a @gen function is different from a trace of @gen function, which contains the return value associated with an execution as well as the assignment to each random choice made during the execution. See Generative Function Interface for more information about traces."
 },
 
 {
@@ -373,7 +373,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Modeling Languages",
     "title": "Static DSL",
     "category": "section",
-    "text": "Static DSL functions are defined using the @staticgen macro. We will refer to Static DSL functions as \'@staticgen functions\' from here forward.Here is an example @staticgen function that samples two random choices:@staticgen function foo(prob::Float64)\n    z1 = @addr(bernoulli(prob), :a)\n    z2 = @addr(bernoulli(prob), :b)\n    return z1 || z2\nendAfter running this code, foo is a Julia value whose type is a subtype of StaticIRGenerativeFunction, which is a subtype of GenerativeFunction.The Static DSL permits a subset of the syntax permitted by the Dynamic DSL. In particular, each statement must be one of the following forms:- `<symbol> = <julia-expr>`\n- `<symbol> = `@addr(<dist>(..),<symbol> [ => ..])`\n- `@addr(<dist>(..),<symbol> [ => ..])`\n- `return <julia-expr>`Note that the @addr keyword may only appear in at the top-level of the right-hand-side expresssion. Also, addresses used with the @addr keyword must be a literal Julia symbol (e.g. :a). If multi-part addresses are used, the first component in the multi-part address must be a literal Julia symbol (e.g. :a = i is valid).Also, symbols used on the left-hand-side of assignment statements must be unique (this is called \'static single assignment\' (SSA) form) (this is called \'static single-assignment\' (SSA) form)."
+    "text": "Static DSL functions are defined using the @staticgen macro. We will refer to Static DSL functions as \'@staticgen functions\' from here forward.Here is an example @staticgen function that samples two random choices:@staticgen function foo(prob::Float64)\n    z1 = @addr(bernoulli(prob), :a)\n    z2 = @addr(bernoulli(prob), :b)\n    return z1 || z2\nendAfter running this code, foo is a Julia value whose type is a subtype of StaticIRGenerativeFunction, which is a subtype of GenerativeFunction.The Static DSL permits a subset of the syntax permitted by the Dynamic DSL. In particular, each statement must be one of the following forms:<symbol> = <julia-expr>\n<symbol> =@addr(<dist|gen-fn>(..),<symbol> [ => ..])`\n@addr(<dist|gen-fn>(..),<symbol> [ => ..])\nreturn <julia-expr>Note that the @addr keyword may only appear in at the top-level of the right-hand-side expresssion. Also, addresses used with the @addr keyword must be a literal Julia symbol (e.g. :a). If multi-part addresses are used, the first component in the multi-part address must be a literal Julia symbol (e.g. :a => i is valid).Also, symbols used on the left-hand-side of assignment statements must be unique (this is called \'static single assignment\' (SSA) form) (this is called \'static single-assignment\' (SSA) form)."
 },
 
 {
@@ -806,6 +806,86 @@ var documenterSearchIndex = {"docs": [
     "title": "Probability Distributions",
     "category": "section",
     "text": ""
+},
+
+{
+    "location": "ref/distributions/#Gen.bernoulli",
+    "page": "Probability Distributions",
+    "title": "Gen.bernoulli",
+    "category": "constant",
+    "text": "bernoulli(prob_true::Real)\n\nSamples a Bool value which is true with given probability\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/distributions/#Gen.normal",
+    "page": "Probability Distributions",
+    "title": "Gen.normal",
+    "category": "constant",
+    "text": "normal(mu::Real, std::Real)\n\nSamples a Float64 value from a normal distribution.\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/distributions/#Gen.mvnormal",
+    "page": "Probability Distributions",
+    "title": "Gen.mvnormal",
+    "category": "constant",
+    "text": "mvnormal(mu::AbstractVector{T}, cov::AbstractMatrix{U}} where {T<:Real,U<:Real}\n\nSamples a Vector{Float64} value from a multivariate normal distribution.\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/distributions/#Gen.gamma",
+    "page": "Probability Distributions",
+    "title": "Gen.gamma",
+    "category": "constant",
+    "text": "gamma(shape::Real, scale::Real)\n\nSample a Float64 from a gamma distribution.\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/distributions/#Gen.inv_gamma",
+    "page": "Probability Distributions",
+    "title": "Gen.inv_gamma",
+    "category": "constant",
+    "text": "inv_gamma(shape::Real, scale::Real)\n\nSample a Float64 from a inverse gamma distribution.\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/distributions/#Gen.beta",
+    "page": "Probability Distributions",
+    "title": "Gen.beta",
+    "category": "constant",
+    "text": "beta(alpha::Real, beta::Real)\n\nSample a Float64 from a beta distribution.\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/distributions/#Gen.categorical",
+    "page": "Probability Distributions",
+    "title": "Gen.categorical",
+    "category": "constant",
+    "text": "categorical(probs::AbstractArray{U, 1}) where {U <: Real}\n\nGiven a vector of probabilities probs where sum(probs) = 1, sample an Int i from the set {1, 2, .., length(probs)} with probability probs[i].\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/distributions/#Gen.uniform",
+    "page": "Probability Distributions",
+    "title": "Gen.uniform",
+    "category": "constant",
+    "text": "uniform(low::Real, high::Real)\n\nSample a Float64 from the uniform distribution on the interval [low, high].\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/distributions/#Gen.uniform_discrete",
+    "page": "Probability Distributions",
+    "title": "Gen.uniform_discrete",
+    "category": "constant",
+    "text": "uniform_discrete(low::Integer, high::Integer)\n\nSample an Int from the uniform distribution on the set {low, low + 1, ..., high-1, high}.\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/distributions/#Gen.poisson",
+    "page": "Probability Distributions",
+    "title": "Gen.poisson",
+    "category": "constant",
+    "text": "poisson(lambda::Real)\n\nSample an Int from the Poisson distribution with rate lambda.\n\n\n\n\n\n"
 },
 
 {
