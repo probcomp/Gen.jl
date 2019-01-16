da9419055dc2631c50e26a3c283f5416b38724da
alex-lew <alexl.mail@gmail.com>
2019-01-15 20:08:31 -0500

    Use 100 iters with bigger step size for MALA example
    Makes examples run faster, brings it into line with the other regression examples in terms of computational effort, and achieves basically the same results.

--- examples/regression/static_mala.jl
+++ examples/regression/static_mala.jl
@@ -29,8 +29,8 @@ function do_inference(xs, ys, num_iters)
     
     scores = Vector{Float64}(undef, num_iters)
     for i=1:num_iters
-        (trace, _) = mala(trace, line_selection, 0.0001)
-        (trace, _) = mala(trace, std_selection, 0.0001)
+        (trace, _) = mala(trace, line_selection, 0.001)
+        (trace, _) = mala(trace, std_selection, 0.001)
     
         # step on the outliers
         for j=1:length(xs)
@@ -53,4 +53,4 @@ end
 
 (xs, ys) = make_data_set(200)
 do_inference(xs, ys, 10)
-@time scores = do_inference(xs, ys, 1000)
+@time scores = do_inference(xs, ys, 100)
