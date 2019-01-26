cd403ff7646db488176cb064cc42a209aca8e0e2
autodocs <autodocs>
2019-01-26 20:41:51 +0000

    build based on f6d9e36

--- dev/search_index.js
+++ dev/search_index.js
@@ -713,11 +713,59 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "ref/combinators/#Gen.Map",
+    "page": "Generative Functions Combinators",
+    "title": "Gen.Map",
+    "category": "type",
+    "text": "gen_fn = Map(kernel::GenerativeFunction)\n\nReturn a new generative function that applies the kernel independently for a vector of inputs.\n\nThe returned generative function has one argument with type Vector{X} for each argument of the input generative function with type X. The length of each argument, which must be the same for each argument, determines the number of times the input generative function is called (N). Each call to the input function is made under address namespace i for i=1..N. The return value of the returned function has type FunctionalCollections.PersistentVector{Y} where Y is the type of the return value of the input function. The map combinator is similar to the \'map\' higher order function in functional programming, except that the map combinator returns a new generative function that must then be separately applied.\n\n\n\n\n\n"
+},
+
+{
     "location": "ref/combinators/#Map-combinator-1",
     "page": "Generative Functions Combinators",
     "title": "Map combinator",
     "category": "section",
-    "text": "The map combinator takes a generative function as input, and returns a generative function that applies the given generative function independently to a vector of arguments. The returned generative function has one argument with type Vector{T} for each argument of type T of the input generative function. The length of each argument, which must be the same for each argument, determines the number of times the input generative function is called (N). Each call to the input function is made under address namespace i for i=1..N. The return value of the returned function has type Vector{T} where T is the type of the return value of the input function. The map combinator is similar to the \'map\' higher order function in functional programming, except that the map combinator returns a new generative function that must then be separately applied.For example, consider the following generative function, which makes one random choice at address :z:@gen function foo(x::Float64, y::Float64)\n    @addr(normal(x + y, 1.0), :z)\nendWe apply the map combinator to produce a new generative function bar:bar = Map(foo)We can then obtain a trace of bar:xs = [0.0, 0.5]\nys = [0.5, 1.0]\n(trace, _) = initialize(bar, (xs, ys))This causes foo to be invoked twice, once with arguments (xs[1], ys[1]) in address namespace 1 and once with arguments (xs[2], ys[2]) in address namespace 2. The resulting trace has random choices at addresses 1 => :z and 2 => :z."
+    "text": "MapIn the schematic below, the kernel is denoted mathcalG_mathrmk.<div style=\"text-align:center\">\n    <img src=\"../../images/map_combinator.png\" alt=\"schematic of map combinator\" width=\"50%\"/>\n</div>For example, consider the following generative function, which makes one random choice at address :z:@gen function foo(x1::Float64, x2::Float64)\n    y = @addr(normal(x1 + x2, 1.0), :z)\n    return y\nendWe apply the map combinator to produce a new generative function bar:bar = Map(foo)We can then obtain a trace of bar:(trace, _) = initialize(bar, ([0.0, 0.5], [0.5, 1.0]))This causes foo to be invoked twice, once with arguments (0.0, 0.5) in address namespace 1 and once with arguments (0.5, 1.0) in address namespace 2. If the resulting trace has random choices:│\n├── 1\n│   │\n│   └── :z : -0.5757913836706721\n│\n└── 2\n    │\n    └── :z : 0.7357177113395333then the return value is:FunctionalCollections.PersistentVector{Any}[-0.575791, 0.735718]"
+},
+
+{
+    "location": "ref/combinators/#Gen.MapCustomArgDiff",
+    "page": "Generative Functions Combinators",
+    "title": "Gen.MapCustomArgDiff",
+    "category": "type",
+    "text": "argdiff = MapCustomArgDiff{T}(retained_argdiffs::Dict{Int,T})\n\nConstruct an argdiff value that contains argdiff information for some subset of applications of the kernel.\n\nIf the number of applications of the kernel, which is determined from the the length of hte input vector(s), has changed, then retained_argdiffs may only contain argdiffs for kernel applications that exist both in the previous trace and and the new trace. For each i in keys(retained_argdiffs), retained_argdiffs[i] contains the argdiff information for the ith application. If an entry is not provided for some i that exists in both the previous and new traces, then its argdiff will be assumed to be NoArgDiff.\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/combinators/#Argdiffs-1",
+    "page": "Generative Functions Combinators",
+    "title": "Argdiffs",
+    "category": "section",
+    "text": "Generative functions produced by this combinator accept the following argdiff types:NoArgDiff\nUnknownArgDiff\nMapCustomArgDiffMapCustomArgDiff"
+},
+
+{
+    "location": "ref/combinators/#Gen.VectorCustomRetDiff",
+    "page": "Generative Functions Combinators",
+    "title": "Gen.VectorCustomRetDiff",
+    "category": "type",
+    "text": "retdiff = VectorCustomRetDiff(retained_retdiffs:Dict{Int,Any})\n\nConstruct a retdiff that provides retdiff information about some elements of the returned vector.\n\nIf the length of the vector has changed, then retained_retdiffs may only contain retdiffs for positions in the vector that exist in both the previous and new vector. For each i in keys(retained_retdiffs), retained_retdiffs[i] contains the retdiff information for the ith position. A missing entry for some i that exists in both the previous and new vectors indicates that its value has not changed.\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/combinators/#Retdiffs-1",
+    "page": "Generative Functions Combinators",
+    "title": "Retdiffs",
+    "category": "section",
+    "text": "Generative functions produced by this combinator may return retdiffs that are one of the following types:NoRetDiff\nVectorCustomRetDiffVectorCustomRetDiff"
+},
+
+{
+    "location": "ref/combinators/#Gen.Unfold",
+    "page": "Generative Functions Combinators",
+    "title": "Gen.Unfold",
+    "category": "type",
+    "text": "gen_fn = Unfold(kernel::GenerativeFunction)\n\nReturn a new generative function that applies the kernel in sequence, passing the return value of one application as an input to the next.\n\nThe kernel accepts the following arguments:\n\nThe first argument is the Int index indicating the position in the sequence (starting from 1).\nThe second argument is the state.\nThe kernel may have additional arguments after the state.\n\nThe return type of the kernel must be the same type as the state.\n\nThe returned generative function accepts the following arguments:\n\nThe number of times (N) to apply the kernel.\nThe initial state.\nThe rest of the arguments (not including the state) that will be passed to each kernel application.\n\nThe return type of the returned generative function is FunctionalCollections.PersistentVector{T} where T is the return type of the kernel.\n\n\n\n\n\n"
 },
 
 {
@@ -725,7 +773,31 @@ var documenterSearchIndex = {"docs": [
     "page": "Generative Functions Combinators",
     "title": "Unfold combinator",
     "category": "section",
-    "text": ""
+    "text": "UnfoldIn the schematic below, the kernel is denoted mathcalG_mathrmk. The initial state is denoted y_0, the number of applications is n, and the remaining arguments to the kernel not including the state, are z.<div style=\"text-align:center\">\n    <img src=\"../../images/unfold_combinator.png\" alt=\"schematic of unfold combinator\" width=\"70%\"/>\n</div>For example, consider the following kernel, with state type Bool, which makes one random choice at address :z:@gen function foo(t::Int, y_prev::Bool, z1::Float64, z2::Float64)\n    y = @addr(bernoulli(y_prev ? z1 : z2), :y)\n    return y\nendWe apply the map combinator to produce a new generative function bar:bar = Map(foo)We can then obtain a trace of bar:(trace, _) = initialize(bar, (5, false, 0.05, 0.95))This causes foo to be invoked five times. The resulting trace may contain the following random choices:│\n├── 1\n│   │\n│   └── :y : true\n│\n├── 2\n│   │\n│   └── :y : false\n│\n├── 3\n│   │\n│   └── :y : true\n│\n├── 4\n│   │\n│   └── :y : false\n│\n└── 5\n    │\n    └── :y : true\nthen the return value is:FunctionalCollections.PersistentVector{Any}[true, false, true, false, true]"
+},
+
+{
+    "location": "ref/combinators/#Gen.UnfoldCustomArgDiff",
+    "page": "Generative Functions Combinators",
+    "title": "Gen.UnfoldCustomArgDiff",
+    "category": "type",
+    "text": "argdiff = UnfoldCustomArgDiff(init_changed::Bool, params_changed::Bool)\n\nConstruct an argdiff that indicates whether the initial state may have changed (init_changed) , and whether or not the remaining arguments to the kernel may have changed (params_changed).\n\n\n\n\n\n"
+},
+
+{
+    "location": "ref/combinators/#Argdiffs-2",
+    "page": "Generative Functions Combinators",
+    "title": "Argdiffs",
+    "category": "section",
+    "text": "Generative functions produced by this combinator accept the following argdiff types:NoArgDiff\nUnknownArgDiff\nUnfoldCustomArgDiffUnfoldCustomArgDiff"
+},
+
+{
+    "location": "ref/combinators/#Retdiffs-2",
+    "page": "Generative Functions Combinators",
+    "title": "Retdiffs",
+    "category": "section",
+    "text": "Generative functions produced by this combinator may return retdiffs that are one of the following types:NoRetDiff\nVectorCustomRetDiff"
 },
 
 {
@@ -733,7 +805,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Generative Functions Combinators",
     "title": "Recurse combinator",
     "category": "section",
-    "text": ""
+    "text": "TODO: document me<div style=\"text-align:center\">\n    <img src=\"../../images/recurse_combinator.png\" alt=\"schematic of recurse combinatokr\" width=\"70%\"/>\n</div>"
 },
 
 {
