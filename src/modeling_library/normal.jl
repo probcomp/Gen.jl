4329ead0d4a25b8f66122289ebd01c63bb194c55
Ben Zinberg <bzinberg@alum.mit.edu>
2019-05-23 00:55:00 -0700

    Fix some broadcasting mistakes, and allow mixtures of scalar-valued arguments and array-valued arguments (but still have a special case for the all-scalar version)

--- src/modeling_library/normal.jl
+++ src/modeling_library/normal.jl
@@ -6,7 +6,6 @@ function broadcast_compatible_or_crash(args...)
     # way to check this, and I think it makes more sense to wait until that
     # capability exists in Julia proper than to re-implement it here.
     s = Base.Broadcast.broadcast_shape(map(size, args)...)
-    true
 end
 
 
@@ -15,12 +14,12 @@ end
 
 Samples a `Float64` value from a normal distribution.
 
-    normal(mu::Array{Real, N}, std::Array{Real, N})
+    normal(mu::Array{T}, std::Array{U}) where {T<:Real, U<:Real}
 
-Samples an `Array{Float64, N}` where each element is independently normally
-distributed.  This is equivalent to a multivariate normal with diagonal
-covariance matrix, but its implementation is more efficient than that of the
-more general `mvnormal` for this case.
+Samples an `Array{Float64}` of shape `broadcast(size(mu), size(std))` where each
+element is independently normally distributed.  This is equivalent to a
+multivariate normal with diagonal covariance matrix, but its implementation is
+more efficient than that of the more general `mvnormal` for this case.
 
 The shapes of `mu` and `std` must be broadcast-compatible.  For methods such as
 `logpdf(x, mu, std)` which involve an element of the support of the
@@ -41,20 +40,33 @@ Float64
 """
 const normal = Normal()
 
+function logpdf(::Normal,
+                x::Union{Array{T}, T},
+                mu::Union{Array{U}, U},
+                std::Union{Array{V}, V}) where {T<:Real, U<:Real, V<:Real}
+    broadcast_compatible_or_crash(x, mu, std)
+    var = std .* std
+    diff = x .- mu
+    -(diff .* diff) ./ (2.0 * var) .- 0.5 * log.(2.0 * pi * var)
+end
+
 function logpdf(::Normal, x::Real, mu::Real, std::Real)
     var = std * std
     diff = x - mu
     -(diff * diff)/ (2.0 * var) - 0.5 * log(2.0 * pi * var)
 end
 
-function logpdf(::Normal,
-                x::Array{T, N},
-                mu::Array{U, N},
-                std::Array{V, N}) where {T<:Real, U<:Real, V<:Real, N}
+function logpdf_grad(::Normal,
+                     x::Union{Array{T}, T},
+                     mu::Union{Array{U}, U},
+                     std::Union{Array{V}, V}) where {T<:Real, U<:Real, V<:Real}
     broadcast_compatible_or_crash(x, mu, std)
-    var = std .* std
-    diff = x - y
-    sum(-(diff .* diff) ./ (2.0 * var) - 0.5 * log.(2.0 * pi * var))
+    precision = 1.0 ./ (std .* std)
+    diff = mu .- x
+    deriv_x = diff .* precision
+    deriv_mu = -deriv_x
+    deriv_std = -1.0 ./ std .+ (diff .* diff) ./ (std .* std .* std)
+    (deriv_x, deriv_mu, deriv_std)
 end
 
 function logpdf_grad(::Normal, x::Real, mu::Real, std::Real)
@@ -66,27 +78,14 @@ function logpdf_grad(::Normal, x::Real, mu::Real, std::Real)
     (deriv_x, deriv_mu, deriv_std)
 end
 
-function logpdf_grad(::Normal,
-                     x::Array{T, N},
-                     mu::Array{U, N},
-                     std::Array{V, N}) where {T<:Real, U<:Real, V<:Real, N}
-    broadcast_compatible_or_crash(x, mu, std)
-    precision = 1.0 ./ (std .* std)
-    diff = mu - x
-    deriv_x = diff .* precision
-    deriv_mu = -deriv_x
-    deriv_std = -1.0 ./ std + (diff .* diff) ./ (std .* std .* std)
-    (deriv_x, deriv_mu, deriv_std)
+function random(::Normal,
+                mu::Union{Array{T}, T},
+                std::Union{Array{U}, U}) where {T<:Real, U<:Real}
+    broadcast_shape = broadcast_compatible_or_crash(mu, std)
+    mu .+ std .* randn(broadcast_shape)
 end
 
 random(::Normal, mu::Real, std::Real) = mu + std * randn()
-
-function random(::Normal,
-                mu::Array{T, N},
-                std::Array{U, N}) where {T<:Real, U<:Real, N}
-    broadcast_compatible_or_crash(mu, std)
-    mu + std .* randn(size(mu))
-end
 
 (::Normal)(mu, std) = random(Normal(), mu, std)
 
