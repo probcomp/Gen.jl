c7b5454841619fade1f2b9f8a92dd5ca474ec1cb
SamWitty <samawitty@gmail.com>
2019-11-08 11:19:01 -0500

    Added covariance gradient test

--- test/modeling_library/distributions.jl
+++ test/modeling_library/distributions.jl
@@ -172,8 +172,8 @@ end
     @test isapprox(actual[1][2], finite_diff_vec(f, args, 1, 2, dx))
     @test isapprox(actual[2][1], finite_diff_vec(f, args, 2, 1, dx))
     @test isapprox(actual[2][2], finite_diff_vec(f, args, 2, 2, dx))
-
-    @test actual[3] === nothing # not yet implemented
+    @test isapprox(actual[3][1, 1], finite_diff_mat_sym(f, args, 3, 1, 1, dx))
+    @test isapprox(actual[3][1, 2], finite_diff_mat_sym(f, args, 3, 1, 2, dx))
 end
 
 @testset "piecewise_uniform" begin
