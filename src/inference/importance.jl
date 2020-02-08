ef2843aef6696e7e0fab96f71ba07091009397ff
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-02-08 15:27:30 -0500

    Document verbose kwarg for `importance_resampling`

--- src/inference/importance.jl
+++ src/inference/importance.jl
@@ -50,16 +50,19 @@ end
 
 """
     (trace, lml_est) = importance_resampling(model::GenerativeFunction,
-        model_args::Tuple, observations::ChoiceMap, num_samples::Int)
+        model_args::Tuple, observations::ChoiceMap, num_samples::Int,
+        verbose=false)
 
     (traces, lml_est) = importance_resampling(model::GenerativeFunction,
         model_args::Tuple, observations::ChoiceMap,
         proposal::GenerativeFunction, proposal_args::Tuple,
-        num_samples::Int)
+        num_samples::Int, verbose=false)
 
 Run sampling importance resampling, returning a single trace.
 
 Unlike `importance_sampling`, the memory used constant in the number of samples.
+
+Setting `verbose=true` prints a progress message every sample.
 """
 function importance_resampling(model::GenerativeFunction{T,U}, model_args::Tuple,
                                observations::ChoiceMap,
