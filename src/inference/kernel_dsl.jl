5c628454b38548f0039f6d7ed32ffa8230008e60
Marco Cusumano-Towner <imarcoam@gmail.com>
2020-02-09 21:45:14 -0500

    Add reversal kernels

--- src/inference/kernel_dsl.jl
+++ src/inference/kernel_dsl.jl
@@ -1,6 +1,7 @@
 import MacroTools
 
-check_is_kernel(::Any) = false
+is_primitive(::Function) = false
+check_is_kernel(::Function) = false
 
 function check_observations(choices::ChoiceMap, observations::ChoiceMap)
     for (key, value) in get_values_shallow(observations)
@@ -16,20 +17,20 @@ macro pkern(ex)
     MacroTools.@capture(ex, function f_(args__) body_ end) || error("expected a function")
     quote
         function $(esc(f))($(args...), check = false, observations = EmptyChoiceMap())
-            trace = $body
+            trace::Trace = $body
             check && check_observations(get_choices(trace), observations)
             trace
         end
+        is_primitive($(esc(f))) = true
         check_is_kernel($(esc(f))) = true
     end
 end
 
-macro kern(ex)
-
+function expand_kern_ex(ex)
     MacroTools.@capture(ex, function f_(args__) body_ end) || error("expected a function")
 
     ex = quote
-        function $(esc(f))(trace, $(args...), check = false, observations = EmptyChoiceMap())
+        function $(esc(f))(trace::Trace, $(args...), check = false, observations = EmptyChoiceMap())
             $body
             check && check_observations(get_choices(trace), observations)
             trace
@@ -94,17 +95,85 @@ macro kern(ex)
 
     # applying a kernel
     ex = MacroTools.postwalk(ex) do x
-        if MacroTools.@capture(x, @app K_(args__))
-            quote
-                check && check_is_kernel($(esc(K)))
-                trace = $(esc(K))(trace, $(args...), check)
-            end
-        else
-            x
+        MacroTools.@capture(x, @app K_(args__)) || return x
+        quote
+            check && check_is_kernel($(esc(K)))
+            trace = $(esc(K))(trace, $(args...), check)
         end
     end
+
+    ex, f
+end
+
+function reversal(f)
+    check_is_kernel(f)
+    error("Reversal for kernel $f is not defined")
+end
+
+macro revkern(ex)
+    MacroTools.@capture(ex, k_ : l_) || error("expected a pair of functions")
+    quote
+        is_primitive($k) || error("first function is not a primitive kernel")
+        is_primitive($l) || error("second function is not a primitive kernel")
+        reversal($k) = $l
+        reversal($l) = $k
+    end
+end
+
+
+function reversal_ex(ex)
+
+    MacroTools.@capture(ex, function f_(args__) body_ end) || error("expected a function")
+
+    # change the name
+    ex = quote
+        function rev($(args...))
+            $body
+        end
+    end
+
+    # for loops
+    ex = MacroTools.postwalk(ex) do x
+        MacroTools.@capture(x, for idx_ in range_ body_ end) || return x
+        quote
+            for $idx in reverse(loop_range)
+                $body
+            end
+        end
+    end
+
+    # applying a kernel
+    ex = MacroTools.postwalk(ex) do x
+        MacroTools.@capture(x, @app K_(args__)) || return x
+        quote
+            @app_ reversal($K)($(args...))
+        end
+    end
+    ex = MacroTools.postwalk(ex) do x
+        MacroTools.@capture(x, @app_ K_(args__)) || return x
+        quote
+            @app $K($(args...))
+        end
+    end
+
 
     ex
 end
 
-export @pkern, @kern
+macro kern(ex)
+    kern_ex, kern = expand_kern_ex(ex)
+    rev_kern_ex, rev_kern = expand_kern_ex(reversal_ex(ex))
+    quote
+        # define forward kerel
+        $kern_ex
+        
+        # define reversal kernel
+        $rev_kern_ex
+
+        # bind the reversals for both
+        Gen.reversal($(esc(kern))) = $rev_kern
+        Gen.reversal($rev_kern) = $(esc(kern))
+    end
+end
+
+export @pkern, @revkern, @kern, reversal
