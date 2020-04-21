8ff66db4f4eaab4dd2f705e7b006317c8abcb1df
Kenta Takatsu <kt426@cornell.edu>
2020-04-21 11:55:41 -0400

    added gradients and tests for dir

--- src/modeling_library/dirichlet.jl
+++ src/modeling_library/dirichlet.jl
@@ -8,17 +8,24 @@ Samples a `Vector{Float64}` value from a dirichlet
 const dirichlet = Dirichlet()
 
 function logpdf(::Dirichlet, x::AbstractArray{T}, alpha::AbstractArray{U}) where {T <: Real, U <: Real}
-    if all(alpha .> 0.)
-        dist = Distributions.Dirichlet(alpha)
-        Distributions.logpdf(dist, x)
+    if !(isapprox(sum(x),1) & all(x .>= 0) & all(x .<= 1))
+        -Inf
+    elseif all(alpha .> 0.)
+        sum((alpha.-1).*log.(x)) - (sum(loggamma.(alpha))-loggamma(sum(alpha)))
     else
         -Inf
     end
 end
 
 function logpdf_grad(::Dirichlet, x::AbstractArray{T}, alpha::AbstractArray{U}) where {T <: Real, U <: Real}
-    error("Not Implemented")
-    (nothing, nothing)
+    if (isapprox(sum(x),1) & all(x .>= 0) & all(x .<= 1))
+        println()
+        deriv_x = sum((alpha.-1) ./ x)
+        deriv_alpha = log.(x) .+ digamma(sum(alpha)) .- digamma.(alpha)
+    else
+        error("x has to be a simplex: $x")
+    end
+    Tuple(vcat([deriv_x, deriv_alpha]...))
 end
 
 function random(::Dirichlet, alpha::AbstractVector{U}) where {U <: Real}
@@ -27,7 +34,7 @@ function random(::Dirichlet, alpha::AbstractVector{U}) where {U <: Real}
     rand(Distributions.Dirichlet(alpha))
 end
 
-
+is_discrete(::Dirichlet) = false
 (::Dirichlet)(alpha) = random(Dirichlet(), alpha)
 
 has_output_grad(::Dirichlet) = false, false
