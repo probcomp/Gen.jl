10298fb772d5863b3e05a6c87ce811c0446c1117
bzinberg <bzinberg@mit.edu>
2020-04-24 15:23:57 -0400

    Document the return value of `maybe_resample!`

--- src/inference/particle_filter.jl
+++ src/inference/particle_filter.jl
@@ -186,6 +186,7 @@ end
         ess_threshold::Float64=length(state.traces)/2, verbose=false)
 
 Do a resampling step if the effective sample size is below the given threshold.
+Return `true` if a resample thus occurred, `false` otherwise.
 """
 function maybe_resample!(state::ParticleFilterState{U};
                         ess_threshold::Real=length(state.traces)/2, verbose=false) where {U}
