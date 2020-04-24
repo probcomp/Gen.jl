8012b15edd44f685cf6073d85529abaee51edc44
autodocs <autodocs>
2020-04-24 20:02:12 +0000

    build based on b433efe

--- dev/search_index.js
+++ dev/search_index.js
@@ -1549,7 +1549,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Particle Filtering",
     "title": "Gen.maybe_resample!",
     "category": "function",
-    "text": "did_resample::Bool = maybe_resample!(state::ParticleFilterState;\n    ess_threshold::Float64=length(state.traces)/2, verbose=false)\n\nDo a resampling step if the effective sample size is below the given threshold.\n\n\n\n\n\n"
+    "text": "did_resample::Bool = maybe_resample!(state::ParticleFilterState;\n    ess_threshold::Float64=length(state.traces)/2, verbose=false)\n\nDo a resampling step if the effective sample size is below the given threshold. Return true if a resample thus occurred, false otherwise.\n\n\n\n\n\n"
 },
 
 {
