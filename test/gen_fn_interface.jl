d9a8ff97a48438956f49961f81c2e5b521a5b165
Ben Zinberg <bzinberg@alum.mit.edu>
2020-10-14 15:15:41 -0400

    Thanks @ztangent for explaining how to make the `let` version work
    Co-authored-by: ztangent <tanqazx@gmail.com>

--- test/gen_fn_interface.jl
+++ test/gen_fn_interface.jl
@@ -1,41 +1,40 @@
-@gen function f_dynamic(x, y)
-    z ~ normal(0, 1)
-    return x + y + z
-end
-@gen (static) function f_static(x, y)
-    z ~ normal(0, 1)
-    return x + y + z
-end
-@load_generated_functions()
+let f_dynamic = (@gen function f(x, y)
+                     z ~ normal(0, 1)
+                     return x + y + z
+                 end),
+    f_static  = (@gen (static) function f(x, y)
+                     z ~ normal(0, 1)
+                     return x + y + z
+                 end)
+    load_generated_functions()
+    for (lang, f) in [:dynamic => f_dynamic,
+                      :static  => f_static]
+        @testset "update(...) shorthand assuming unchanged args ($lang modeling lang)" begin
+            trace0 = simulate(f, (5, 6))
 
-for (lang, f) in [:dynamic => f_dynamic,
-                  :static  => f_static]
-    @testset "update(...) shorthand assuming unchanged args ($lang modeling lang)" begin
-        trace0 = simulate(f, (5, 6))
-
-        constraints = choicemap((:z, 0))
-        trace1, _, _, discard = update(trace0, constraints)
-        # The main test is that the shorthand version runs without crashing,
-        # which is already shown by the time we get here.  Beyond that, let's
-        # sanity-check that `update` did what it's supposed to.
-        @test get_args(trace1) == (5, 6)
-        @test trace1[:z] == 0
-        @test :z in keys(get_values_shallow(discard))
-    end
-
-    @testset "regenerate(...) shorthand assuming unchanged args ($lang modeling lang)" begin
-        @gen function f(x, y)
-            z ~ normal(0, 1)
-            return x + y + z
+            constraints = choicemap((:z, 0))
+            trace1, _, _, discard = update(trace0, constraints)
+            # The main test is that the shorthand version runs without crashing,
+            # which is already shown by the time we get here.  Beyond that, let's
+            # sanity-check that `update` did what it's supposed to.
+            @test get_args(trace1) == (5, 6)
+            @test trace1[:z] == 0
+            @test :z in keys(get_values_shallow(discard))
         end
-        trace0 = simulate(f, (5, 6))
 
-        trace1, _, _ = regenerate(trace0, select(:z))
-        # The main test is that the shorthand version runs without crashing,
-        # which is already shown by the time we get here.  Beyond that, let's
-        # sanity-check that `regenerate` did what it's supposed to.
-        @test get_args(trace1) == (5, 6)
-        @test trace1[:z] != trace0[:z]
+        @testset "regenerate(...) shorthand assuming unchanged args ($lang modeling lang)" begin
+            @gen function f(x, y)
+                z ~ normal(0, 1)
+                return x + y + z
+            end
+            trace0 = simulate(f, (5, 6))
+
+            trace1, _, _ = regenerate(trace0, select(:z))
+            # The main test is that the shorthand version runs without crashing,
+            # which is already shown by the time we get here.  Beyond that, let's
+            # sanity-check that `regenerate` did what it's supposed to.
+            @test get_args(trace1) == (5, 6)
+            @test trace1[:z] != trace0[:z]
+        end
     end
 end
-
