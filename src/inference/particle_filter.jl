f7adc7281a8d61e39214c58667ed8e3fee1ef1e2
jac lou hoo <postylem@users.noreply.github.com>
2021-04-07 13:02:41 -0400

    docstring: added missing comma

--- src/inference/particle_filter.jl
+++ src/inference/particle_filter.jl
@@ -71,7 +71,7 @@ end
 
 """
     state = initialize_particle_filter(model::GenerativeFunction, model_args::Tuple,
-        observations::ChoiceMap proposal::GenerativeFunction, proposal_args::Tuple,
+        observations::ChoiceMap, proposal::GenerativeFunction, proposal_args::Tuple,
         num_particles::Int)
 
 Initialize the state of a particle filter using a custom proposal for the initial latent state.
