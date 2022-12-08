a9ea16bb986e1a2d0f048b428c3c329405fb93fa
Feras A. Saad <fsaad@cmu.edu>
2022-12-07 20:58:44 -0500

    Fix tyop.

--- src/inference/trace_translators.jl
+++ src/inference/trace_translators.jl
@@ -23,7 +23,7 @@ end
 """
     pair_bijections!(f1::TraceTransformDSLProgram, f2::TraceTransformDSLProgram)
 
-Assert that a pair of bijections contsructed using the [Trace Transform DSL](@ref) are
+Assert that a pair of bijections constructed using the [Trace Transform DSL](@ref) are
 inverses of one another.
 """
 function pair_bijections!(f1::TraceTransformDSLProgram, f2::TraceTransformDSLProgram)
